82320a3c686290e74be3aef33dbdb38654541ffd
JONAAVILA <jona_1969@hotmail.com>
2025-01-08 09:57:23 -0300

    slider created

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import './Slider.css';
+import images from './images';
+
+const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [ visibleImages, setVisibleImages ] = useState(3)
+  const interval = 3000
+  const screen = window.innerWidth
+
+  const goToNextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex >= images.length - visibleImages ? 0 : prevIndex + 1))
+  };
+  
+  useEffect(()=>{ 
+    if(screen <= 500){
+      setVisibleImages(1)
+    }
+    if(screen >= 750 && screen <= 900 ){
+      setVisibleImages(3)
+    }
+  },[screen])
+
+  useEffect(() => {
+    const autoPlay = setInterval(goToNextSlide, interval)
+    return () => clearInterval(autoPlay)
+  }, [currentIndex, interval])
+
+  const displayedImages = images.slice(currentIndex, currentIndex + visibleImages)
+
+  return (
+    <div className="carousel">
+      <div className="slide-container">
+        {displayedImages.map((image, index) => (
+          <div key={index} className="slide">
+            <img src={image} alt={`Slide ${currentIndex + index}`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Slider;
