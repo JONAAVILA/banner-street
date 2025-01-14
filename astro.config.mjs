9db571d161cf2bf553558ab1c116cfbd7a22f65e
JONAAVILA <jona_1969@hotmail.com>
2025-01-14 10:30:01 -0300

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pasacalleslamancha.com.ar',
   integrations: [
     react(),
     sitemap({
-      hostname:'https://pasacalleslamancha.com.ar'
+      hostname:'https://pasacalleslamancha.com.ar',
+      lastmod: new Date(),
+      priority: 1,
     })
   ]
 });
