110e47ae1ba3e4632659f3d98133d7a1a7fb8ccc
JONAAVILA <jona_1969@hotmail.com>
2025-01-14 10:07:13 -0300

    dependencies to create sitemap file is installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react(),
+    sitemap({
+      hostname:'https://pasacalleslamancha.com.ar'
+    })
+  ]
 });
